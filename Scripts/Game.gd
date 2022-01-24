949c6eb48e0535299f5359843518c573f5b1b7e1
TimotheeMB <60016090+TimotheeMB@users.noreply.github.com>
2022-01-24 16:11:01 +0100

    ouille ouille

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,12 +2,18 @@ extends Node2D
 
 func _ready():
 	get_tree().connect("network_peer_connected",self,"create_player")
+	get_tree().connect("connected_to_server",self,"_connected_to_server")
 	if Net.is_host:
 		create_player(get_tree().get_network_unique_id())
+		
 func create_players():
 	for id in Net.peer_ids:
 		create_player(id)
 	create_player(Net.net_id)
+	
+func _connected_to_server() -> void:
+	yield(get_tree().create_timer(0.1), "timeout")
+	create_player(get_tree().get_network_unique_id())
 
 func create_player(id):
 	#print("Player with ID " + str(id) + " initialized")
