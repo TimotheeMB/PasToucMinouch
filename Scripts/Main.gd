7e306b9d6762dc79f04e35ddd4e2d5ccf710a7e0
TimotheeMB <60016090+TimotheeMB@users.noreply.github.com>
2022-01-25 17:35:22 +0100

    enemies

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,6 +4,8 @@ extends Node2D
 var player = preload("res://Scenes/Player.tscn")
 var enemy = preload("res://Scenes/Enemy.tscn")
 
+var nb_of_player := 1
+
 func _ready() -> void:
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	randomize()
@@ -12,12 +14,14 @@ func _ready() -> void:
 
 func _player_connected(id) -> void:
 	create_player(id)
+	nb_of_player += 1
 
 
 func create_player(id):
 	var p = player.instance()
 	add_child(p)
 	p.initialize(id)
+
 
 
 func _on_Host_pressed():
@@ -33,6 +37,12 @@ func _on_Join_pressed():
 
 
 func _on_Timer_timeout():
-	var e = enemy.instance()
-	add_child(e)
-	e.position = Vector2(randf()*300,randf()*300)
+	if Net.is_host:
+		var e = enemy.instance()
+		add_enemy(e)
+		e.position = Vector2(randf()*300,randf()*300)
+		if nb_of_player > 1:
+			rpc("add_enemy",e)
+
+func add_enemy(enemy):
+	$Enemies.add_child(enemy)
